bdaa78f02f01b4fa92c0020ff76c9ee8f7234e32
tudicodings <tudor.apavaloaie@stud.ubbcluj.ro>
2024-04-27 00:54:10 +0300

    iteriatia 1 - 80% interfata

--- src/main/java/com/example/angvanz/HelloController.java
+++ src/main/java/com/example/angvanz/HelloController.java
@@ -16,17 +16,34 @@ public class HelloController {
     @FXML
     private Label welcomeText;
 
+    boolean admin = false;
+    boolean agent  = false;
+
+    //PENTRU LOGARE
     @FXML
     private TextField userTF;
     @FXML
     private TextField passTF;
+
+    // PENTRU CREEARE COMANDA
+    @FXML
+    private TextField idCom;
+    @FXML
+    private TextField numeF;
+    @FXML
+    private TextField denum;
+    @FXML
+    private TextField cant;
+
+    // BUTOANE
     @FXML 
     Button button;
-
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    Button comanda;
+    @FXML
+    Button back;
+    @FXML
+    Button add;
 
     @FXML
     private void openAdministrare(){
@@ -36,19 +53,109 @@ public class HelloController {
             if(userU.isEmpty() || passU.isEmpty()){
                 throw new NumberFormatException("All fields must be filled.");
             }
-            if(!passU.endsWith("123")){
+            if(passU.endsWith("1")){
+                Alert alert = new Alert(Alert.AlertType.WARNING, "Sunteti logat ca admin");
+                alert.show();
+                admin = true;
+                agent = false;
+            }else if(passU.endsWith("2")){
+                Alert alert = new Alert(Alert.AlertType.WARNING, "Sunteti logat ca agent");
+                alert.show();
+                agent = true;
+                admin = false;
+            }
+            //((!passU.endsWith("1") && passU.endsWith("2")) ||( !passU.endsWith("2") && passU.endsWith("1")))
+            if(!passU.endsWith("1") && !passU.endsWith("2")){
                 throw new NumberFormatException("Wrong password");
             }
             FXMLLoader loader = new FXMLLoader(getClass().getResource("administrare.fxml"));
             Parent root = loader.load();
             Scene scene = new Scene(root);
             Stage stage = (Stage) button.getScene().getWindow();
-            stage.setTitle("Agent de vanzari");
+            stage.setTitle("Meniu.");
             stage.setScene(scene);
             stage.show();
         }catch (Exception e){
             Alert alert = new Alert(Alert.AlertType.WARNING, e.getMessage());
             alert.show();
+        }
+    }
+
+    @FXML
+    private void openCreeareComanda(){
+        try{
+            if(!admin){
+                throw new NumberFormatException("Not an admin!");
+            }
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("creeare-comanda.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) comanda.getScene().getWindow();
+            stage.setTitle("Creeare comanda.");
+            stage.setScene(scene);
+            stage.show();
+        }catch (Exception e){
+            Alert alert = new Alert(Alert.AlertType.WARNING, e.getMessage());
+            alert.show();
+        }
+    }
+
+    @FXML
+    private void back1(){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) back.getScene().getWindow();
+            stage.setTitle("Panel logare");
+            stage.setScene(scene);
+            stage.show();
+        }catch (Exception e){
+            Alert alert = new Alert(Alert.AlertType.WARNING, e.getMessage());
+            alert.show();
+        }
+    }
+
+    @FXML
+    private void back2(){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("administrare.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) back.getScene().getWindow();
+            stage.setTitle("Meniu.");
+            stage.setScene(scene);
+            stage.show();
+        }catch (Exception e){
+            Alert alert = new Alert(Alert.AlertType.WARNING, e.getMessage());
+            alert.show();
+        }
+    }
+    @FXML
+    private void addComanda(){
+        try {
+            var idc = idCom.getText();
+            //var idcomanda = Integer.parseInt(idCom.getText());
+            var numeFirma = numeF.getText();
+            var denumireP =  denum.getText();
+            //var cantitateC = Integer.parseInt(cant.getText());
+            var cantc = cant.getText();
+            if ( idc.isEmpty() || numeFirma.isEmpty() || denumireP.isEmpty() || cantc.isEmpty()) {
+                throw new NumberFormatException("You can't leave empty fields!");
+            }
+        }catch (Exception e){
+            Alert alert = new Alert(Alert.AlertType.WARNING, e.getMessage());
+            alert.show();
+        }
+    }
+    @FXML
+    private void addComandaFinal(){
+        try{
+            throw new NumberFormatException("Comanda a fost adaugata!");
+        }catch (Exception e){
+            Alert alert = new Alert(Alert.AlertType.WARNING, e.getMessage());
+            alert.show();
+
         }
     }
 }
